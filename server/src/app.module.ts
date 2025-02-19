5d57cee64cce7c8eb9e349c4e64592c2f41080a7
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-19 16:23:35 +0900

    chore: (server) TypeORM 환경설정 및 MySQL DB 연결

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // config 모듈을 통한 process.env 객체 사용
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true, // 개발환경에서만 true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
