48c2522ead43d28702027216b74530d657ba9f17
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-19 14:13:50 +0900

    Feat: form 관련 공통 TextField 컴포넌트 및 useForm hook 구현 (#1)
    * feat: 공통 TextField 컴포넌트 UI 구현
    
    * feat: form input을 관리하는 useForm hook 구현
    
    * rename: TextField 컴포넌트 파일 이름 수정
    
    * feat: 로그인 페이지 form 구현

---
+++ src/components/TextField.tsx
@@ -0,0 +1,26 @@
+import { InputHTMLAttributes } from 'react'
+
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  isError?: boolean
+  errorMessage?: string
+}
+
+export default function TextField({
+  isError = false,
+  errorMessage = '',
+  ...props
+}: TextFieldProps) {
+  return (
+    <div className="relative">
+      <div className="rounded-sm border border-slate-200 focus-within:outline-1 focus-within:outline-indigo-500">
+        <input
+          autoComplete="off"
+          className="w-full rounded-sm p-2 text-base outline-none"
+          {...props}
+        />
+      </div>
+
+      {isError && <p className="p-1 text-sm text-red-600">{errorMessage}</p>}
+    </div>
+  )
+}
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,43 @@
+import TextField from '../components/TextField'
+import useForm from '../hooks/useForm'
+
 export default function LoginPage() {
-  return <div>로그인 페이지</div>
+  const { inputs, getTextInputProps } = useForm({
+    initialValue: {
+      id: '',
+      password: '',
+    },
+  })
+
+  return (
+    <form className="flex w-96 flex-col gap-4 p-4">
+      <h1 className="text-xl text-indigo-800">로그인</h1>
+
+      <div className="flex flex-col justify-center gap-2">
+        <label htmlFor="id" className="text-sm text-slate-600">
+          아이디
+        </label>
+        <TextField
+          id="id"
+          autoFocus
+          placeholder="아이디를 입력해주세요."
+          {...getTextInputProps('id')}
+          isError
+          errorMessage="아이디를 입력해주세요."
+        />
+      </div>
+
+      <div className="flex flex-col justify-center gap-2">
+        <label htmlFor="password" className="text-sm text-slate-600">
+          비밀번호
+        </label>
+        <TextField
+          id="password"
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          {...getTextInputProps('password')}
+        />
+      </div>
+    </form>
+  )
 }
