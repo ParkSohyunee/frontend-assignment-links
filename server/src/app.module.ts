01aab940b453e7a2def4fad8bff9a78d5752f2e7
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-20 19:03:11 +0900

    feat: (server) 카테고리 조회 api 구현

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true, // 개발환경에서만 true
     }),
     AuthModule,
+    CategoryModule,
   ],
   providers: [ConfigService],
 })
---
+++ server/src/category/category.entity.ts
@@ -0,0 +1,17 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+
+@Entity()
+@Unique(['name'])
+export class Category extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
