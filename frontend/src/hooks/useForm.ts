e735f3f39c5661398aea1e62c22cb0b02baa8d18
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-20 14:30:19 +0900

    feat: useForm에 validate 로직 추가

--- frontend/src/hooks/useForm.ts
+++ frontend/src/hooks/useForm.ts
@@ -1,11 +1,17 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 interface UseFormProps<T> {
   initialValue: T
+  validate: (inputs: T) => Record<keyof T, string>
 }
 
-export default function useForm<T>({ initialValue }: UseFormProps<T>) {
+export default function useForm<T>({
+  initialValue,
+  validate,
+}: UseFormProps<T>) {
   const [inputs, setInputs] = useState(initialValue)
+  const [touched, setTouched] = useState<Record<string, boolean>>({})
+  const [errors, setErrors] = useState<Record<string, string>>({})
 
   const handleChangeInputs = (name: keyof T, text: string) => {
     setInputs((prev) => ({
@@ -14,20 +20,38 @@ export default function useForm<T>({ initialValue }: UseFormProps<T>) {
     }))
   }
 
+  const handleBlur = (name: keyof T) => {
+    setTouched((prev) => ({
+      ...prev,
+      [name]: true,
+    }))
+  }
+
   const getTextInputProps = (name: keyof T) => {
     const input = inputs[name]
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
       handleChangeInputs(name, e.target.value)
     }
+    const onBlur = () => {
+      handleBlur(name)
+    }
 
     return {
       input,
       onChange,
+      onBlur,
     }
   }
 
+  useEffect(() => {
+    const errors = validate(inputs)
+    setErrors(errors)
+  }, [inputs, validate])
+
   return {
     inputs,
+    touched,
+    errors,
     getTextInputProps,
   }
 }
--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,1 +1,1 @@
-// Utils
+export * from './validate'
