d12ae69d3528471ac3244719a403f75889558127
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-20 13:24:18 +0900

    chore: CORS 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // CORS 설정
+    // (참고 - Nest.js Docs) https://docs.nestjs.com/security/cors
+    cors: {
+      origin: 'http://localhost:5173', // 요청을 허용할 origin
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   console.log('서버 실행');
 }
