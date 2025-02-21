e9485da345b0cbdbc89f6020f31f1a76596bc419
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-21 10:44:36 +0900

    feat: 테이블간 관계 설정

--- server/src/category/category.entity.ts
+++ server/src/category/category.entity.ts
@@ -1,7 +1,9 @@
+import { Link } from 'src/link/link.entity';
 import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
@@ -14,4 +16,8 @@ export class Category extends BaseEntity {
 
   @Column()
   name: string;
+
+  // --OneToMany 하나의 카테고리는 여러개의 링크를 가질 수 있다.
+  @OneToMany(() => Link, (link) => link.category, { eager: false })
+  links: Link[];
 }
--- server/src/link/link.entity.ts
+++ server/src/link/link.entity.ts
@@ -1,13 +1,25 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { Category } from 'src/category/category.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Link extends BaseEntity {
   @PrimaryGeneratedColumn() // 기본키
   id: number;
 
-  // TODO User 테이블과 연결 필요
-  // @Column()
-  // createdBy: number;
+  // --@ManyToOne 링크는 한명의 사용자를 갖는다.
+  // --@JoinColumn은 외래 키(FK)의 컬럼명을 명시하는 역할, 즉 외래키가 테이블의 createdById 컬럼과 연결된다.
+  // --@JoinColumn은 외래 키가 위치하는 테이블의 필드에만 사용해야 한다.
+  @ManyToOne(() => User, (user) => user.links, { eager: false })
+  @JoinColumn({ name: 'createdById' })
+  createdBy: User;
 
   @Column()
   name: string;
@@ -15,7 +27,9 @@ export class Link extends BaseEntity {
   @Column()
   url: string;
 
-  // TODO Category 테이블과 연결 필요
-  @Column()
-  categoryId: number;
+  // --@ManyToOne 링크는 하나의 카테고리를 갖는다.
+  // --@JoinColumn은 외래 키(FK)가 테이블의 categoryId 컬럼과 연결된다.
+  @ManyToOne(() => Category, (category) => category.links, { eager: false })
+  @JoinColumn({ name: 'categoryId' })
+  category: Category;
 }
