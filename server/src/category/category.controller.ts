fb8150a296ea52036d5de38634d47160b320247e
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-02-21 15:30:16 +0900

    feat: 카테고리 조회 기능에 AuthGuard 추가

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
 import { CategoryService } from './category.service';
+
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('categories')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   categories() {
     return this.categoryService.categories();
   }
--- server/src/category/category.module.ts
+++ server/src/category/category.module.ts
@@ -5,8 +5,10 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { Category } from './category.entity';
 
+import { AuthModule } from 'src/auth/auth.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category]), AuthModule],
   providers: [CategoryService],
   controllers: [CategoryController],
 })
